d516cd9023340d51c0933d7bf3448343359d3bd7
Ujjwal <ujjwal.srivastava@laitkor.com>
2025-02-10 15:21:35 +0530

    updated manifest

--- addons/rewrite_urls/__manifest__.py
+++ addons/rewrite_urls/__manifest__.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 {
     'name': "rewrite_urls",
-
+    'version': '1.0',    
     'summary': "Redirect SEO friendly URLs to the original URLs with this module",
-
     'description': """
 This module allows us to map SEO friendly URLs with the original URLs.
     """,
-
     'author': "Laitkor Consultancy Services Pvt. Ltd.",
     'category': 'Website',
-    'version': '1.0',
-
     'depends': ['base', 'website', 'product', 'web'],
-
     'data': [
         'security/ir.model.access.csv',
         'views/seo_friendly_url_views.xml',
     ],
+    'installable':True,
+    'application':True,
+    'auto_install':False
 }
 
