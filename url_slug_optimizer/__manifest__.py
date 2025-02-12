bc1f9b716e76836680cc08ad14457cbfd70bbd84
Ujjwal <ujjwal.srivastava@laitkor.com>
2025-02-12 13:17:01 +0530

    change module name to url_slug_optimizer

---
+++ url_slug_optimizer/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "url_slug_optimizer",
+    'summary': "Redirect SEO friendly URLs to the original URLs with this module",
+    'description': """
+    This module allows us to map SEO friendly URLs with the original URLs.
+    """,
+    'price': 1.99,
+    'currency': 'EUR',
+    'author': "Laitkor Consultancy Services Pvt. Ltd.",
+    'category': 'Website',
+    'version': '17.0',
+    'depends': ['base', 'website', 'product', 'web'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/seo_friendly_url_views.xml',
+    ],
+}
